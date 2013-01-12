12ba00ba0da4c684c8c56f606b63bb034c2aa8c8
Matthew J. Roberts <mjobobs@gmail.com>
2013-01-12 17:30:59 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catfeeder::Application.config.secret_token = 'fefcf63143d97cedeb3055dd5144b9ea617b16b1a9ec9a7079605ca96b40e7e81bc48263860444541dcc276fa9a127d6b017998f8a753b92a143e52ba818d697'
